0d96f0490f9a84e5aa85bbdfb92ebdd5e78fb85a
Bojan Ilic <bi4528@student.uni-lj.si>
2020-12-04 22:20:26 +0100

    Registreacija html

--- view/registracija.php
+++ view/registracija.php
@@ -4,6 +4,19 @@
         <title>Registracija</title>
     </head>
     <body>
-        <p>Hello registracija</p>
+        <h1> Registracija </h1>
+        <form action="" method="post">
+            <input type="hidden" name="id" />
+            Ime: <input type="text" name="ime" /><br />
+            Priimek: <input type="text" name="priimek"  /><br />
+            Elektronski naslov: <input type="text" name="email"  /><br />
+            Geslo: <input type="text" name="geslo" /><br />
+            Ponovitev gesla: <input type="text" name="pgeslo"  /><br />
+            <input type="radio" id="prodajalec" name="vloga" value="prodajalec">
+            <label for="prodajalec">Prodajalec</label>
+            <input type="radio" id="stranka" name="vloga" value="stranka">
+            <label for="stranka">Stranka</label><br>
+            <p><button>Pošlji</button></p>
+        </form>
     </body>
 </html>
