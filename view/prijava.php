9def80337ec08f3950b237104d6041684ae5fd35
Bojan Ilic <bi4528@student.uni-lj.si>
2020-12-04 22:22:55 +0100

    Prijava html

--- view/prijava.php
+++ view/prijava.php
@@ -4,6 +4,12 @@
         <title>Prijava</title>
     </head>
     <body>
-        <p>hello prijava lala</p>
+        <h1> Prijava </h1>
+        <form action="" method="post">
+            <input type="hidden" name="id" />
+            Elektronski naslov: <input type="text" name="email"  /><br />
+            Geslo: <input type="text" name="geslo" /><br />
+            <p><button>Pošlji</button></p>
+        </form>
     </body>
 </html>
