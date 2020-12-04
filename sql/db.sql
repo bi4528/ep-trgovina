3e3f18df9088c33b2b8b4a1ef6214e22a0bdb45f
Bojan Ilic <bi4528@student.uni-lj.si>
2020-12-04 19:53:45 +0100

    JokeDB prekopiran v UporabnikDB in db.sql spremenjen

--- sql/db.sql
+++ sql/db.sql
@@ -1,20 +1,22 @@
--- shema jokes
-DROP DATABASE IF EXISTS jokes;
-CREATE DATABASE jokes;
-USE jokes;
+-- shema uporabniki
+DROP DATABASE IF EXISTS uporabiniki;
+CREATE DATABASE uporabniki;
+USE uporabniki;
 
--- tabela jokes
+-- tabela uporabniki
 
-DROP TABLE IF EXISTS `jokes`;
-CREATE TABLE `jokes` (
+DROP TABLE IF EXISTS `uporabniki`;
+CREATE TABLE `uporabniki` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `joke_text` text,
-  `joke_date` date NOT NULL,
+  `ime` text,
+  `priimek` text,
+  `email` text,
+  `geslo` text,
+  `naslov` text,
+  `vloga` text,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- vnosi v tabelo
-INSERT INTO `jokes` (joke_text, joke_date) VALUES ('Chuck Norris can write infinite recursion functions ... and have them return.', NOW());
-INSERT INTO `jokes` (joke_text, joke_date) VALUES ('Chuck can hit you so hard your web app will turn into swing application.', NOW());
-INSERT INTO `jokes` (joke_text, joke_date) VALUES ('The functions Chuck Norris writes have no arguments, because nobody argues with Chuck Norris.', NOW());
+INSERT INTO `uporabniki` (ime, priimek, email, geslo, vloga) VALUES ('Adam', 'Admin', 'adam.admin@mail.com', 'adamAmin123', 'admin');
 
