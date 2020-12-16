04dda80180e1376b660052bcd00839dc4cc08316
Bojan Ilic <bi4528@student.uni-lj.si>
2020-12-16 12:21:54 +0100

    pa zakljčeni oddaji se pobriše košarica

--- view/oddaja.php
+++ view/oddaja.php
@@ -3,9 +3,11 @@
 if (isset($_SESSION["id"])) {
    
     echo ' <p> Pozdravljen ' . $_SESSION["ime"] . ', Vaše naročilo je bilo uspešno oddano. </p> ';
-    echo ' <p> Status vašega naročila si laho ogledate <a href="' . BASE_URL . "seznamNarocil" . '">tukaj.</a> </p> ';
-    
     ?>
+    <form action="<?= BASE_URL . "cart" ?>" method="post">
+                <input type="hidden" name="do" value="purge_cart">
+                 <p> Status vašega naročila si lahko ogledate <input href="seznamNarocil" type="submit" value="tukaj."></p> 
+    </form>
     <form action="<?= BASE_URL . "cart" ?>" method="post">
                 <input type="hidden" name="do" value="purge_cart">
                  <p> Z nakupom lahko nadaljujete <input href="izdelki" type="submit" value="tukaj."></p> 
